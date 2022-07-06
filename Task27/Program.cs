77b3623679b9fb3a29e8eee47e339fa84f02d23d
Andrey Drozdov <drozdov_andrey@list.ru>
2022-07-06 17:00:41 +0300

    task 27 done

---
+++ Task27/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+namespace GB
+{
+    class Task27
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.Clear();
+            Console.WriteLine("Введите число:");
+            string a = Convert.ToString(Console.ReadLine());
+            int result = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                result = result + Convert.ToInt32(a[i].ToString());
+            }
+            Console.WriteLine(result);
+            
+        }  
+    }
+}
